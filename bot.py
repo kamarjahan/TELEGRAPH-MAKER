b78d6a500ca7643521fa6dbbfadb50f5871b1977
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-02-20 16:29:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,4 @@
-#    This file is part of the ChannelAutoForwarder distribution (https://github.com/Benchamxd/Telegraph-Uploader).
-#    Copyright (c) 2021 Rithunand
-#    
-#    This program is free software: you can redistribute it and/or modify  
-#    it under the terms of the GNU General Public License as published by  
-#    the Free Software Foundation, version 3.
-# 
-#    This program is distributed in the hope that it will be useful, but 
-#    WITHOUT ANY WARRANTY; without even the implied warranty of 
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-#    General Public License for more details.
-# 
-#    License can be found in < https://github.com/MoTechYT/MT-Telegraph-Uploader/blob/main/License> 
+
 
 import os
 from telegraph import upload_file
