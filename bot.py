e7f68562096aba275206cecc6d78d3f28a958637
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-02-21 18:46:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,7 @@ from pyrogram.types import (
     InlineKeyboardMarkup, InlineKeyboardButton,
     CallbackQuery, InlineQuery, Message)
 import logging
+import true
 
 devourdevils = Client(
    "Telegra.ph Uploader",
