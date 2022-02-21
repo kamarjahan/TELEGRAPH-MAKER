1e1764b84bc4253e86ea765f5d75596c93d0786b
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-02-21 21:56:07 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -7,3 +7,9 @@ class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 12345))
 
     API_HASH = os.environ.get("API_HASH", "")
+    
+    DATABASE_URL = os.environ.get("DB_URL", "")
+    
+    DATABASE_NAME = os.environ.get("DB_NAME", "")
+    
+    
