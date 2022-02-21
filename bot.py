a2c942db6756dac4e6dd78259fae64f71b8411d7
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-02-21 14:06:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -256,13 +256,17 @@ GITHUB PRO:</b> <a href="github.com/kamarjahan">GITHUB </a>
 WHO ASKED DEV INFO :{message.from_user.mention}"""
     
     
-            disable_web_page_preview=True,        
-            parse_mode="html")
+
     
         
     
     
     await message.reply_text(text=text)
+    
+    
+    
+            disable_web_page_preview=True,        
+            parse_mode="html")
           
 
 
