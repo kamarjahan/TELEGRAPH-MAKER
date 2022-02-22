8f8d23092dcfbf8f56b54f17eaf7f7858741c018
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-02-22 22:14:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -305,7 +305,7 @@ WHO ASKED DEV INFO :{message.from_user.mention}""", disable_web_page_preview=Tru
     
 @devourdevils.on_message(filters.command("cr")) 
 async def cr(client, message):
-    text = f"""hello {message.from_user.mention}  fool of the the day command maded for folling you my boy
+    text=f"""hello {message.from_user.mention}  fool of the the day command maded for folling you my boy
     this cmd find by </b> <a href="T.ME/devourdevils">DEVOURDEVILS </a>""", disable_web_page_preview=True,
     
     
