1d59f43ac5104181f477163c5b3da3d9fd89d1a8
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-02-22 08:50:13 +0530

    Update log.py

--- log.py
+++ log.py
@@ -1,5 +1,6 @@
 import logging
 from tglogging import TelegramLogHandler
+import setuptools
 
 # TelegramLogHandler is a custom handler which is inherited from an existing handler. ie, StreamHandler.
 
@@ -21,3 +22,39 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 logger.info("live log streaming to telegram.")
+
+
+
+
+
+
+
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="tglogging",
+    version="0.0.3",
+    author="subinps",
+    description="A python package to stream your app logs to a telegram chat in realtime.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Tracker": "https://github.com/subinps/tglogging/issues",
+        "Source": "https://github.com/subinps/tglogging",
+    },
+    license="MIT",
+    url="https://github.com/subinps/tglogging",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        'aiohttp',
+        'nest_asyncio'
+    ]
+)
