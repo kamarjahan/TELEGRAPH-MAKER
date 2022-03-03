f16ad902edbc844125badfd8077411ab78782f01
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-03 20:36:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,13 @@ from pyrogram.types import (
     InlineKeyboardMarkup, InlineKeyboardButton,
     CallbackQuery, InlineQuery, Message)
 import logging
+from config import BOT_USERNAME
+from driver.decorators import check_blacklist
+from driver.filters import command
+from driver.database.dbpunish import is_gbanned_user
+from youtube_search import YoutubeSearch
+
+
 
 devourdevils = Client(
    "Telegra.ph Uploader",
@@ -294,6 +301,38 @@ WHO ASKED DEV INFO :{message.from_user.mention}""", disable_web_page_preview=Tru
         
          
     )
+
+
+
+
+
+@Client.on_message(command(["search", f"search@{BOT_USERNAME}"]))
+@check_blacklist()
+async def ytsearch(_, message: Message):
+    if len(message.command) < 2:
+        return await message.reply_text("/search **ɴᴇᴇᴅs ᴀɴ ᴀʀɢᴜᴍᴇɴᴛ !**")
+    query = message.text.split(None, 1)[1]
+    m = await message.reply_text("🔎 **sᴇᴀʀᴄʜɪɴɢ...**")
+    results = YoutubeSearch(query, max_results=5).to_dict()
+    text = ""
+    for i in range(5):
+        try:
+            text += f"🏷 **Name:** __{results[i]['title']}__\n"
+            text += f"⏱ **Duration:** `{results[i]['duration']}`\n"
+            text += f"👀 **Views:** `{results[i]['views']}`\n"
+            text += f"📣 **Channel:** {results[i]['channel']}\n"
+            text += f"🔗: https://www.youtube.com{results[i]['url_suffix']}\n\n"
+        except IndexError:
+            break
+    await m.edit_text(
+        text,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🗑 Close", callback_data="close")]]
+        ),
+    )
+
+
    
   
 
