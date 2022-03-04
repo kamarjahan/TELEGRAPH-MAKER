4fc138388732a26d033f1cc689fe016a3ff8b0f2
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-04 07:56:56 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -8,3 +8,27 @@ from pyrogram.types import (
     InlineKeyboardMarkup, InlineKeyboardButton,
     CallbackQuery, InlineQuery, Message)
 import logging
+
+
+@devourdevils.on_callback_query()
+async def button(Tgraph, update):
+      cb_data = update.data
+      if "help" in cb_data:
+        await update.message.delete()
+     
+        await help(Tgraph, update.message)
+      elif "close" in cb_data:
+        await update.message.delete() 
+      elif "home" in cb_data:
+        await update.message.delete()
+        await home(Tgraph, update.message)
+      elif "help" in cb_data:
+        await update.message.delete()
+        await help(Tgraph, update.message)
+        await help(Tgraph, update.message)
+      elif "cmd" in cb_data:
+        await update.message.delete()
+        await cmd(Tgraph, update.message)   
+      
+
+devourdevils.run()
