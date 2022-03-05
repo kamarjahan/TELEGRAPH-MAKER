9603ca6d18bf290793d292338ef022c075275e87
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-05 20:32:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -150,12 +150,7 @@ async def home(client, message):
         InlineKeyboardButton('Close🔐', callback_data='close')
     ],
     [
-        InlineKeyboardButton('🗣️Any Doubt', url='http://telegram.me/devourdevils'),
-        InlineKeyboardButton('Source Code📃', url='https://github.com/kamarjahan/TELEGRAPH-MAKER')
-    ],
-    [
-        InlineKeyboardButton('Dev', url='t.me/devourdevils'),
-        InlineKeyboardButton('support ChAT', url='t.me/septemberfilms')
+
     ]]
   reply_markup = InlineKeyboardMarkup(buttons)
   await devourdevils.send_photo(
@@ -207,6 +202,9 @@ async def help(client, message):
     [
         InlineKeyboardButton('⚕️Our group⚕️', url='t.me/septemberfilms'),
         InlineKeyboardButton('cmd', callback_data='cmd')
+    ],
+    [   
+        InlineKeyboardButton('bot info', callback_data='botinfo')
     ]]
   reply_markup = InlineKeyboardMarkup(buttons)
   await devourdevils.send_photo(
@@ -243,8 +241,6 @@ ALL_PIC = [
  "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
 ]
 
-
-
 @devourdevils.on_message(filters.command(["cmd"]))
 async def cmd(client, message):
   buttons = [[
@@ -252,14 +248,15 @@ async def cmd(client, message):
         InlineKeyboardButton('Close', callback_data='close'),
         InlineKeyboardButton('id', callback_data='id'),
         InlineKeyboardButton('dev', callback_data='dev'),
-        InlineKeyboardButton('name', callback_data='name')
+        InlineKeyboardButton('name', callback_data='name'),
+        InlineKeyboardButton('botinfo', callback_data='botinfo')
     ],
     [
         InlineKeyboardButton('Help', callback_data='help'),
         InlineKeyboardButton('mention', callback_data='mention'),
         InlineKeyboardButton('telegraph', callback_data='tgraph'),
-        InlineKeyboardButton('username', callback_data='username')
-        
+        InlineKeyboardButton('username', callback_data='username'),
+        InlineKeyboardButton('about', callback_data='about')
     ]]
   reply_markup = InlineKeyboardMarkup(buttons)
   await devourdevils.send_photo(
@@ -539,6 +536,97 @@ async def name(client, message):
         reply_to_message_id=message.message_id
       )   
   
+  
+
+ALL_PIC = [
+ "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
+ "https://telegra.ph/file/b308d89346393bae36e67.jpg",
+ "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
+ "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
+ "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
+ "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
+ "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
+ "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
+ "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
+ "https://telegra.ph/file/1721beb890958fea346ba.jpg",
+ "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
+ "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
+ "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
+ "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
+ "://telegra.ph/file/66af3aff75335ebdfe049.jpg",
+ "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
+ "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
+]
+
+
+@devourdevils.on_message(filters.command(["botinfo"]))
+async def botinfo(client, message):
+  buttons = [[
+        InlineKeyboardButton('back⏪', callback_data='home'),
+        InlineKeyboardButton('Close', callback_data='close')
+    ],
+    [
+        InlineKeyboardButton('about', callback_data='about')
+    ]]
+  reply_markup = InlineKeyboardMarkup(buttons)
+  await devourdevils.send_photo(
+        photo=random.choice(ALL_PIC),
+        chat_id=message.chat.id,
+        caption=f"""hello bro/sis this is the bot information""",
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=message.message_id
+      )   
+
+ALL_PIC = [
+ "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
+ "https://telegra.ph/file/b308d89346393bae36e67.jpg",
+ "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
+ "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
+ "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
+ "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
+ "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
+ "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
+ "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
+ "https://telegra.ph/file/1721beb890958fea346ba.jpg",
+ "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
+ "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
+ "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
+ "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
+ "://telegra.ph/file/66af3aff75335ebdfe049.jpg",
+ "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
+ "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
+]
+
+@devourdevils.on_message(filters.command(["about"]))
+async def about(client, message):
+  buttons = [[
+        InlineKeyboardButton('back⏪', callback_data='botinfo'),
+        InlineKeyboardButton('Close', callback_data='close')
+    ],
+    [
+        InlineKeyboardButton('devoleper', url='http://telegram.me/devourdevils'),
+        InlineKeyboardButton('deploy own', url='https://heroku.com/deploy?template=https://github.com/kamarjahan/TELEGRAPH-MAKER')
+    ]]
+  reply_markup = InlineKeyboardMarkup(buttons)
+  await devourdevils.send_photo(
+        photo=random.choice(ALL_PIC),
+        chat_id=message.chat.id,
+        caption=f"""this is the about of this bot
+MY NAME:TELEGRAPH BOT
+CREATOR:@DEVOURDEVILS
+LIBRARY:PYROGRAM
+LANGUAGE:PYTHON 3
+DATABASE:MONGO DB
+BOT SERVER:HEROKU
+BUILD STATUS:V1.0.2 [SIM]""",
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=message.message_id
+      )   
+
+
+    
 
 @devourdevils.on_callback_query()
 async def button(Tgraph, update):
@@ -575,7 +663,12 @@ async def button(Tgraph, update):
       elif "username" in cb_data:
         await update.message.delete()
         await username(Tgraph, update.message)
-
+      elif "botinfo" in cb_data:
+        await update.message.delete()
+        await botinfo(Tgraph, update.message)
+      elif "about" in cb_data:
+        await update.message.delete()
+        await about(Tgraph, update.message)
 
 
 
