a7f889a1f16db865f1b23c66f4a7c9ada181e815
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-08 07:59:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -625,44 +625,56 @@ BUILD STATUS:V1.0.2 [SIM]""",
 async def button(Tgraph, update):
       cb_data = update.data
       if "help" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await help(Tgraph, update.message)
       elif "close" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete() 
       elif "home" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await home(Tgraph, update.message)
       elif "help" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await help(Tgraph, update.message)
       elif "cmd" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await cmd(Tgraph, update.message)
       elif "dev" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await dev(Tgraph, update.message)
       elif "id" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await id(Tgraph, update.message)
       elif "mention" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await mention(Tgraph, update.message)
       elif "tgraph" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await telegraph(Tgraph, update.message)
       elif "name" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await name(Tgraph, update.message)
       elif "username" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await username(Tgraph, update.message)
       elif "botinfo" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await botinfo(Tgraph, update.message)
       elif "about" in cb_data:
+        await update.answer("JOIN @SEPTEMBERFILMS")
         await update.message.delete()
         await about(Tgraph, update.message)
-        await update.answer("telegraph bot")
       elif update.data == "alert":
         await update.answer("telegraph bot")
 
