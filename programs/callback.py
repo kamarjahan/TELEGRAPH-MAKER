56dc50350a9ba683cdd4646cf47b8c7870806723
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-08 20:17:14 +0530

    Update callback.py

--- programs/callback.py
+++ programs/callback.py
@@ -7,7 +7,7 @@ from pyrogram.types import (
     InlineQueryResultArticle, InputTextMessageContent,
     InlineKeyboardMarkup, InlineKeyboardButton,
     CallbackQuery, InlineQuery, Message)
-
+from commands import cmd
 
 
 
