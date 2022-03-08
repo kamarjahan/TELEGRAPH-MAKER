cfa23155620e672e739132b9486fbda3c877136e
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-08 18:38:48 +0530

    Create welcome.py

---
+++ programs/welcome.py
@@ -0,0 +1,25 @@
+import os
+from telegraph import upload_file
+import pyrogram
+from pyrogram import filters, Client
+from sample_config import Config
+from pyrogram.types import (
+    InlineQueryResultArticle, InputTextMessageContent,
+    InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery, InlineQuery, Message)
+import logging
+
+
+
+
+
+@Client.on_message(filters.new_chat_members)
+async def welcome(bot,message):
+	chatid= message.chat.id
+	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+	
+@Client.on_message(filters.left_chat_member)
+async def goodbye(bot,message):
+	chatid= message.chat.id
+	await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
+	
