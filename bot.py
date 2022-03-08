950e6efd055372d969fc2439adab081f1d77477e
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-08 18:25:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+import os
+from pyrogram import Client
 
+
+
+devourdevils = Client(
+   "Telegra.ph Uploader",
+   api_id=Config.APP_ID,
+   api_hash=Config.API_HASH,
+   bot_token=Config.TG_BOT_TOKEN,
+   plugins=dict(root="programs")
+)
+
+
+devourdevils.run()
