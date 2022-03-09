fe4805741c8495306e3c49b3b81fa1a35d5639bf
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-09 08:07:20 +0530

    Update filters.py

--- programs/filters.py
+++ programs/filters.py
@@ -2,6 +2,6 @@ import os
 from pyrogram import client, filters
 
 
-@Client.on_message(filter.regex(hi))
+@client.on_message(filter.regex(hi))
 async def hi(bot, msg):
     await msg.reply_text("hi bro")
