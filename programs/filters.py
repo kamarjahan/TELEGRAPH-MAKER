76a2022c4b27f393d8643cb93b52604be454900c
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-09 08:11:33 +0530

    Update filters.py

--- programs/filters.py
+++ programs/filters.py
@@ -1,1 +1,13 @@
+import io
+from pyrogram import filters, Client
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from database.filters_mdb import(
+   add_filter,
+   get_filters,
+   delete_filter,
+   count_filters
+)
 
+from database.connections_mdb import active_connection
+from utils import get_file_id, parser, split_quotes
+from info import ADMINS
