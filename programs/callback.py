7ef35b8368bbbf31310c0175ebde6cb089f2a499
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-10 19:56:35 +0530

    Update callback.py

--- programs/callback.py
+++ programs/callback.py
@@ -35,6 +35,15 @@ async def button(Tgraph, update):
         await help(Tgraph, update.message)
       elif "close" in cb_data:
         await update.message.edit(
+            text="want to realy close the current position"
+            reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton("yes", callback_data='mainclose')
+            InlineKeyboardButton("no", callback_data='home')
+            ]]
+            )
+     
+        )
+        await update.message.edit(
             text="███25%"
         )
         await update.message.edit(
