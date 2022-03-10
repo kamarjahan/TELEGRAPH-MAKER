82652b5f9ca36a8227784ddb8519747df22155ee
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-10 20:10:35 +0530

    Update callback.py

--- programs/callback.py
+++ programs/callback.py
@@ -37,9 +37,9 @@ async def button(Tgraph, update):
         await update.message.edit(
             text="want to realy close the current position"
             reply_markup=InlineKeyboardMarkup( [[
-            InlineKeyboardButton("yes", callback_data='mainclose')
-            InlineKeyboardButton("no", callback_data='home')
-            ]]
+                InlineKeyboardButton("yes", callback_data='mainclose'),
+                InlineKeyboardButton("no", callback_data='home')
+                ]]
             )
      
         )
