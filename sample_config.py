7b4324800e997b4c8b8f5bfdaf3a97d4ce2020ff
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-16 20:08:05 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,17 +1,8 @@
-import os
 
-
-class Config(object):
-    TG_BOT_TOKEN = "5195945385:AAGxrtvKw-ZDJbq_1MLgmxnG2ThyTGvasCQ"
-
-    APP_ID = "17875613"
-
-    API_HASH = "6798f54a7f74e94f2ef0923fba8a8377"
-
-    TG_BOT_USERNAME = "ddtelegraphbot"
-
-    START_MESSAGE = "hi"
-
-    DATABASE_URI = "mongodb+srv://k:k@cluster0.aeenl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    DATABASE_NAME = 'telegraph'
-    COLLECTION_NAME = 'telegram_files'  # If you are using the same database, then use different collection name for each bot
+    TG_BOT_USERNAME = os.environ.get("TG_BOT_USERNAME", "")
+    
+    START_MESSAGE = os.environ.get("START_MESSAGE", "")
+    
+    DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+    DATABASE_NAME = 'Telegram'
+    COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each 
