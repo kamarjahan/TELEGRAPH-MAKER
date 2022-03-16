f51b7f3c21d353517b2e9b604e22d7d2dca17d21
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-16 19:22:01 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,16 +2,16 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = "5195945385:AAGxrtvKw-ZDJbq_1MLgmxnG2ThyTGvasCQ"
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = "17875613"
 
-    API_HASH = os.environ.get("API_HASH", "")
-    
-    TG_BOT_USERNAME = os.environ.get("TG_BOT_USERNAME", "")
-    
-    START_MESSAGE = os.environ.get("START_MESSAGE", "")
-    
-    DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-    DATABASE_NAME = 'Telegram'
-    COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+    API_HASH = "6798f54a7f74e94f2ef0923fba8a8377"
+
+    TG_BOT_USERNAME = "ddtelegraphbot"
+
+    START_MESSAGE = "hi"
+
+    DATABASE_URI = "mongodb+srv://k:k@cluster0.aeenl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    DATABASE_NAME = 'telegraph'
+    COLLECTION_NAME = 'telegram_files'  # If you are using the same database, then use different collection name for each bot
