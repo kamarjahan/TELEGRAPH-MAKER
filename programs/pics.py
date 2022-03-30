904236bf544d7ebd0638d41e0daf06e9d1544604
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-30 21:24:12 +0530

    Create pics.py

---
+++ programs/pics.py
@@ -0,0 +1,27 @@
+import random
+import os
+
+
+
+
+
+
+ALL_PIC = [
+ "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
+ "https://telegra.ph/file/b308d89346393bae36e67.jpg",
+ "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
+ "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
+ "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
+ "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
+ "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
+ "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
+ "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
+ "https://telegra.ph/file/1721beb890958fea346ba.jpg",
+ "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
+ "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
+ "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
+ "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
+ "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
+ "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
+ "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
+]
