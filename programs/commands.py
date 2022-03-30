2e47ef0170d263c75756e741beeee010e6f4d55b
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-30 21:26:05 +0530

    Update commands.py

--- programs/commands.py
+++ programs/commands.py
@@ -9,28 +9,11 @@ from pyrogram.types import (
     CallbackQuery, InlineQuery, Message)
 import logging
 import random
+from programs.pics import ALL_PIC
 
 
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
+
 
     
 @Client.on_message(filters.command(["start"]))
@@ -64,25 +47,6 @@ Made With Love By</code> </b> <a href="t.me/devourdevils">DEVOURDEVIL </a>""",
     )
 
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
 
 
 @Client.on_message(filters.command(["help"]))
@@ -112,25 +76,7 @@ i'll upload it to telegra.ph and give you the direct link**</code>""",
     )
 
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
+
 
 @Client.on_message(filters.command(["cmd"]))
 async def cmd(client, message):
@@ -167,25 +113,7 @@ async def cmd(client, message):
         reply_to_message_id=message.message_id
       ) 
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
+
 
 
 @Client.on_message(filters.command(["id"]))
@@ -207,25 +135,7 @@ async def id(client, message):
         reply_to_message_id=message.message_id
       )
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
+
 
 
 @Client.on_message(filters.command(["dev"]))
@@ -252,25 +162,6 @@ WHO ASKED DEV INFO :{message.from_user.mention}""",
         reply_to_message_id=message.message_id
       )
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
 
 
 @Client.on_message(filters.command(["telegraph"]))
@@ -292,25 +183,6 @@ async def telegraph(client, message):
         reply_to_message_id=message.message_id
     )
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
 
 @Client.on_message(filters.command(["mention"]))
 async def mention(client, message):
@@ -331,25 +203,7 @@ async def mention(client, message):
         reply_to_message_id=message.message_id
       )
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
+
 
 @Client.on_message(filters.command(["username"]))
 async def username(client, message):
@@ -370,25 +224,7 @@ async def username(client, message):
         reply_to_message_id=message.message_id
       )
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
+
 
 
 @Client.on_message(filters.command(["name"]))
@@ -411,25 +247,7 @@ async def name(client, message):
       )   
   
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
+
 
 @Client.on_message(filters.command(["botinfo"]))
 async def botinfo(client, message):
@@ -449,25 +267,7 @@ async def botinfo(client, message):
         reply_to_message_id=message.message_id
       )   
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
+
 
 @Client.on_message(filters.command(["about"]))
 async def about(client, message):
@@ -498,25 +298,7 @@ BUILD STATUS:</code>V2.0.0 [edit]</code>""",
 
 
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
+
 
 @Client.on_message(filters.command(["status"]))
 async def status(client, message):
@@ -551,25 +333,7 @@ BOT ADMINS:</code>452</code>
         reply_to_message_id=message.message_id
       )   
 
-ALL_PIC = [
- "https://telegra.ph/file/5150076a5e8d3ea3de995.jpg",
- "https://telegra.ph/file/b308d89346393bae36e67.jpg",
- "https://telegra.ph/file/c9e6e4ed8ad3269aca2bd.jpg",
- "https://telegra.ph/file/e02dad176eeca63fa83bf.jpg",
- "https://telegra.ph/file/37fd55f07a4670db6c2c6.jpg",
- "https://telegra.ph/file/3b150ca1e1fa3998f4479.jpg",
- "https://telegra.ph/file/5418df1528c0c466c8cda.jpg",
- "https://telegra.ph/file/9682513b3a019f58e4f4d.jpg",
- "https://telegra.ph/file/eebd8bf520218f0fe53e8.jpg",
- "https://telegra.ph/file/1721beb890958fea346ba.jpg",
- "https://telegra.ph/file/a36eb74ffd8357b1e4cd8.jpg",
- "https://telegra.ph/file/7fd76e2190cfde57e4739.jpg",
- "https://telegra.ph/file/9f0334896310bb22bdcb1.jpg",
- "https://telegra.ph/file/3b274bc5398df81dcd820.jpg",
- "https://telegra.ph/file/66af3aff75335ebdfe049.jpg",
- "https://telegra.ph/file/02763d802fcaa64aa2d27.jpg",
- "https://telegra.ph/file/e15f827c3e06982023ac7.jpg"
-]
+
 
 @Client.on_message(filters.command(["corona"]))
 async def corona(client, message):
