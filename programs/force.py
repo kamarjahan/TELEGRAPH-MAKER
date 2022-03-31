edbd68bc2352ce7db8f0fe3d9ffc6e1ed01bb785
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-03-31 22:12:53 +0530

    Update force.py

--- programs/force.py
+++ programs/force.py
@@ -7,25 +7,23 @@ force_channel = "septemberfilms"
 
 
 @Client.on_message(filters.command(["force"]))
-acync def force(client, msg):
-        if force_channel:
+acync def force(bot, msg):
+    if force_channel:
         try:
-            user = await client.get_chat_member(force_channel, msg.from_user.id)
+            user = await bot.get_chat_member(force_channel, msg.from_user.id)
             if user.status == "you removed":
                 await msg.reply_text("you are banned")
                 return
         except UserNotParticipant:
-             await msg.reply_text(
-                 text="your not sub my grp",
-                 reply_markup=InlineKeyboardMarkup( [[
-                  InlineKeyboardButton("join update", url=f"t.me/{force_channel}")
-                  ]]
-                 )
-            )
-            return
-    await msg.reply_text(
-        text="hi"
-)
+            await msg.reply_text(
+                text="your not sub my grp",
+                reply_markup=InlineKeyboardMarkup( [[
+                 InlineKeyboardButton("join update", url=f"t.me/{force_channel}")
+                 ]]
+                )
+           )
+           return
+    await msg.reply_text("hi")
 
 
 
