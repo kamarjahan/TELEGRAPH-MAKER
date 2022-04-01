b5890928aa4ff703bcd40e56469cf7332314fd94
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-04-01 09:28:05 +0530

    Update force.py

--- programs/force.py
+++ programs/force.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
 from pyrogram import filters, Client
 
-force_channel = "ababababaabb"
+force_channel = "septemberfilms"
 
 
 @Client.on_message(filters.command(["fv"]))
