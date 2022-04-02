2949af8501f11d7d68170b373eb5c524366174fb
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-04-02 18:09:37 +0530

    Update trl.py

--- programs/donttouch/trl.py
+++ programs/donttouch/trl.py
@@ -1,6 +1,17 @@
 import os
+from telegraph import upload_file
 import pyrogram
-from pyrogram.types import Message
+from pyrogram import filters, Client
+from sample_config import Config
+from pyrogram.types import (
+    InlineQueryResultArticle, InputTextMessageContent,
+    InlineKeyboardMarkup, InlineKeyboardButton,
+    CallbackQuery, InlineQuery, Message)
+import logging
+import random
+from programs.pics import ALL_PIC
+from pyrogram.errors import UserNotParticipant
+from programs.force import force_channel
 
 
 
