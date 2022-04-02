3fb2f3ec99b76cc598c5c8c02dc95b2869200e6b
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-04-02 21:59:28 +0530

    Update locals.py

--- programs/locals.py
+++ programs/locals.py
@@ -17,5 +17,5 @@ from programs.force import force_channel
 
 @Client.on_message(filters.command([df]))
 async def fg(bot, msg):
-  await reply_text=("hi")
+  await msg.reply_text("your request send to mg db reply soon as possible")
   Message.delete()
