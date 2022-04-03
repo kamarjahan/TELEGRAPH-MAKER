9ce5d5c2ca1a55c22d798cad03c6e40e67adc611
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-04-03 15:14:30 +0530

    Update force.py

--- programs/force.py
+++ programs/force.py
@@ -5,7 +5,7 @@ from pyrogram.types import (
     CallbackQuery, InlineQuery, Message)
 from pyrogram.errors import UserNotParticipant
 from pyrogram import filters, Client
-from programs.donttouch.tr import USERNAME
+from programs.donttouch.trl import USERNAME
 
 #errro
 
