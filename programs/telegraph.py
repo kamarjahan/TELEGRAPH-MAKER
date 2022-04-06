a70df9fdc80bee7e022be72ef61c8661acffaac1
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-04-06 14:02:05 +0530

    Update telegraph.py

--- programs/telegraph.py
+++ programs/telegraph.py
@@ -16,35 +16,8 @@ import logging
 
 
 
-@Client.on_message(filters.photo)
-async def uploadphoto(client, message):
-  msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ download.....join` @septemberfilms")
-  userid = str(message.chat.id)
-  img_path = (f"./Download....!/{userid}.jpg")
-  img_path = await client.download_media(message=message, file_name=img_path)
-  await msg.edit_text("`Uploading.....`")
-  try:
-    tlink = upload_file(img_path)
-  except:
-    await msg.edit_text("`Something went wrong join` @septemberfilms for reply") 
-  else:
-    await msg.edit_text(f"`https://telegra.ph{tlink[0]}`")     
-    os.remove(img_path) 
-            
 
-@Client.on_message(filters.animation)
-async def uploadgif(client, message):
-  if(message.animation.file_size < 5242880):
-    msg = await message.reply_text("`Tʀʏɪɴɢ Tᴏ Dᴏᴡɴʟᴏᴀᴅ join` @septemberfilms")
-    userid = str(message.chat.id)
-    gif_path = (f"./DOWNLOADS/{userid}.mp4")
-    gif_path = await client.download_media(message=message, file_name=gif_path)
-    await msg.edit_text("`Tʀʏɪɴɢ Tᴏ Uᴘʟᴏᴀᴅ.....join` @septemberfilms")
-    try:
-      tlink = upload_file(gif_path)
-      await msg.edit_text(f"`https://telegra.ph{tlink[0]}`")   
-      os.remove(gif_path)   
-    except:import os
+
 from telegraph import upload_file
 import pyrogram
 from pyrogram import filters, Client
