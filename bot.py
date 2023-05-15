ccd00e0a1f38197fcd99a9700f2f91435a9416fd
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2023-05-15 11:51:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,6 @@
-import os
-from pyrogram import Client
-from sample_config import Config
-
-
-
-Devourdevils = Client(
-   "Telegra.ph Uploader",
-   api_id=Config.APP_ID,
-   api_hash=Config.API_HASH,
-   bot_token=Config.TG_BOT_TOKEN,
-   plugins=dict(root="programs")
-)
-
-print("your bot started well or wot")
-
-Devourdevils.run()
+from pyrogram import Client, Filters
+app = Client("my_account")
+@app.on_message(Filters.private)
+def hello(client, message):
+message.reply("Hello {}".format(message.from_user.first_name))
+app.run()
