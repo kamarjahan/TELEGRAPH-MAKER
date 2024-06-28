a5e24d1481aaa195be9fa29c4533b8aac9fa134c
KAMARJAHAN <ztenkammu@gmail.com>
2024-06-29 00:05:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,18 @@
-
 import os
-from pyrogram import Client
-from sample_config import Config
+from pyrogram import Client  # Import the Client class from pyrogram
+from sample_config import Config  # Import the Config from sample_config
 
-
-
+# Initialize the Pyrogram Client
 Devourdevils = Client(
-   "Telegra.ph Uploader",
-   api_id=Config.APP_ID,
-   api_hash=Config.API_HASH,
-   bot_token=Config.TG_BOT_TOKEN,
-   plugins=dict(root="programs")
+    "Telegra.ph Uploader",  # Session name
+    api_id=Config.APP_ID,  # Telegram API ID
+    api_hash=Config.API_HASH,  # Telegram API Hash
+    bot_token=Config.TG_BOT_TOKEN,  # Telegram Bot Token
+    plugins=dict(root="programs")  # Plugins directory
 )
 
-print("your bot started well or wot")
+# Print a message indicating that the bot has started
+print("Your bot started successfully.")
 
+# Run the bot
 Devourdevils.run()
