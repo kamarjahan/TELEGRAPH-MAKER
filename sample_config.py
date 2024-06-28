2d352d18cf78a3d19323ea4db0dddc8af6bd6116
KAMARJAHAN <ztenkammu@gmail.com>
2024-06-28 23:30:48 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -20,15 +20,15 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = "5195945385:AAGxrtvKw-ZDJbq_1MLgmxnG2ThyTGvasCQ"
-    APP_ID = "17875613"
+    TG_BOT_TOKEN = '5195945385:AAGxrtvKw-ZDJbq_1MLgmxnG2ThyTGvasCQ'
+    APP_ID = '17875613'
 
-    API_HASH = "6798f54a7f74e94f2ef0923fba8a8377"
+    API_HASH = '6798f54a7f74e94f2ef0923fba8a8377'
     
-    TG_BOT_USERNAME = "ddtelegraphbot"
+    TG_BOT_USERNAME = 'ddtelegraphbot'
     
 
     
     
-    USERNAME = "devourdevils"
+    USERNAME = 'devourdevils'
 
