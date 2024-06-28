33b651d8ec302d1f93d28f8691ae77273412401f
KAMARJAHAN <ztenkammu@gmail.com>
2024-06-28 23:06:24 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,16 +1,34 @@
+#import os
+
+
+#class Config(object):
+  #  TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+ #   APP_ID = int(os.environ.get("APP_ID", 12345))
+
+  #  API_HASH = os.environ.get("API_HASH", "")
+    
+ #   TG_BOT_USERNAME = os.environ.get("TG_BOT_USERNAME", "")
+    
+ #   START_MESSAGE = os.environ.get("START_MESSAGE", "")
+    
+    
+ #   USERNAME = os.environ.get("USERNAME", "")
+
+
 import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = 5195945385:AAGxrtvKw-ZDJbq_1MLgmxnG2ThyTGvasCQ 
+    APP_ID = 17875613
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    API_HASH = 6798f54a7f74e94f2ef0923fba8a8377
+    
+    TG_BOT_USERNAME = ddtelegraphbot
+    
 
-    API_HASH = os.environ.get("API_HASH", "")
-    
-    TG_BOT_USERNAME = os.environ.get("TG_BOT_USERNAME", "")
-    
-    START_MESSAGE = os.environ.get("START_MESSAGE", "")
     
     
-    USERNAME = os.environ.get("USERNAME", "")
+    USERNAME = devourdevils
+
